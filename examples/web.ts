304c149ef994f4cf191da034632232085b709dbf
Harminder Virk <virk.officials@gmail.com>
2024-12-13 16:04:43 +0530

    style: remove unused import

--- examples/web.ts
+++ examples/web.ts
@@ -7,7 +7,6 @@
  * file that was distributed with this source code.
  */
 
-import cookie from 'cookie'
 import { createServer } from 'node:http'
 import { createError } from '@poppinss/exception'
 
